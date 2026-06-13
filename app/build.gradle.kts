dcf891a450579b03203c1809724807c868b46069
simonpojok <simonpojok@gmail.com>
2026-06-13 22:47:08 +0300

    MOBI-010: added some dixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -140,6 +140,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.material)
     implementation(libs.hilt.android)
     implementation(libs.osmdroid)
     implementation(libs.androidx.compose.foundation)
